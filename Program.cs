628711e017e780ac8a04ca3cc011e018788967d4
apowel <austingpowel@gmail.com>
2020-01-26 21:09:12 -0500

    Using reflection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Lab8
 {
@@ -14,10 +16,10 @@ namespace Lab8
             while (again == true)
             {
                 Console.WriteLine("Which student would you like to learn more about? " +
-                    "(enter a number 1-" + studentDirectory.Directory.Count() + "): ");
+                    "(enter a number 1-" + studentDirectory.directory.Count() + "): ");
                 Student selectedStudent = SelectStudent(studentDirectory);
                 Console.WriteLine("What would you like to know about this student? " +
-                    @"enter ""hometown"", or ""favorite food"": ");
+                    @"enter ""hometown"", ""favorite food"", ""id"", or ""name"": ");
                 SelectInfo(selectedStudent);
                 again = Again();
             }
@@ -35,14 +37,14 @@ namespace Lab8
             catch(FormatException e)
             {
                 Console.WriteLine(e.StackTrace);
-                Console.WriteLine("enter a number 1-"+ studentDirectory.Directory.Count());
+                Console.WriteLine("enter a number 1-"+ studentDirectory.directory.Count());
                 return SelectStudent(studentDirectory);
             }
             catch(OverflowException e)
             {
                 Console.WriteLine(e.StackTrace);
                 Console.WriteLine(e.Message);
-                Console.WriteLine("enter a number 1-" + studentDirectory.Directory.Count());
+                Console.WriteLine("enter a number 1-" + studentDirectory.directory.Count());
                 return SelectStudent(studentDirectory);
             }
 
@@ -61,7 +63,7 @@ namespace Lab8
         }
         public static void SelectInfo(Student selectedStudent)
         {
-            string info;
+            string info = "";
             try
             {
                 info = Console.ReadLine().ToLower().Trim();
@@ -72,24 +74,9 @@ namespace Lab8
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
                 Console.WriteLine("That input broke something.");
-                throw;
-            }
-            
-            if (info == "hometown")
-            {
-                Console.WriteLine(selectedStudent.Name + " is from " + 
-                    selectedStudent.HomeTown + ".");
-            }
-            else if (info == "favorite food")
-            {
-                Console.WriteLine("The favorite food of " + selectedStudent.Name + 
-                    " is " + selectedStudent.Food);
-            }
-            else
-            {
-                Console.WriteLine("Please try again.");
                 SelectInfo(selectedStudent);
             }
+            selectedStudent.GetInfo(info);
         }
         public static bool Again()
         {
@@ -109,6 +96,7 @@ namespace Lab8
             }
             if (againInput == "yes")
             {
+                Console.Clear();
                 return true;
             }
             else if (againInput == "no")
@@ -126,23 +114,48 @@ namespace Lab8
             public int Id { get; set; }
             public string Name { get; set; }
             public string HomeTown { get; set; }
-            public string Food { get; set; }
+            public string FavoriteFood { get; set; }
+
+            //This method exists to allow for easier implementation of new Student properties.
+            public void GetInfo(string info)
+            {
+                PropertyInfo[] properties = this.GetType().GetProperties();
+                bool infoAgain = true;
+                string infoNoSpace = Regex.Replace(info, @"\s+", "");
+                foreach (var property in properties)
+                {
+                    string[] propertyName = property.ToString().Split();
+                    if (propertyName[1].ToString().ToLower() == infoNoSpace)
+                    {
+                        Console.WriteLine("The " + info + " of " + this.Name + " is " +
+                        this.GetType().GetProperty(propertyName[1]).GetValue(this, null).ToString());
+                        infoAgain = false;
+                    }
+                }
+                while(infoAgain)
+                {
+                    Console.WriteLine("That was not a valid input");
+                    infoAgain = false;
+                    SelectInfo(this);
+                }
+            }
         }
+
 
         public class StudentDirectory
         {
-            public List<Student> Directory = new List<Student>()
+            public List<Student> directory = new List<Student>()
             {
-                new Student() { Id = 1, Name = "Jake Collins", HomeTown = "Corona, CA", Food = "Sushi"},
-                new Student() { Id = 2, Name = "Andrew Waltman", HomeTown = "Grand Rapids, MI", Food = "Burgers"},
-                new Student() { Id = 3, Name = "Albert Ngoudjou", HomeTown = "Bafoussam", Food = "Lasagna"},
-                new Student() { Id = 4, Name = "Tommy Waalkes", HomeTown = "Raleigh, NC", Food = "Chicken Curry"},
-                new Student() { Id = 5, Name = "Austin Powel", HomeTown = "Blissfield, MI", Food = "Spaghetti"},
-                new Student() { Id = 6, Name = "Dylan Rule", HomeTown = "Newport, NH", Food = "Poutine"},
+                new Student() { Id = 1, Name = "Jake Collins", HomeTown = "Corona, CA", FavoriteFood = "Sushi"},
+                new Student() { Id = 2, Name = "Andrew Waltman", HomeTown = "Grand Rapids, MI", FavoriteFood = "Burgers"},
+                new Student() { Id = 3, Name = "Albert Ngoudjou", HomeTown = "Bafoussam", FavoriteFood = "Lasagna"},
+                new Student() { Id = 4, Name = "Tommy Waalkes", HomeTown = "Raleigh, NC", FavoriteFood = "Chicken Curry"},
+                new Student() { Id = 5, Name = "Austin Powel", HomeTown = "Blissfield, MI", FavoriteFood = "Spaghetti"},
+                new Student() { Id = 6, Name = "Dylan Rule", HomeTown = "Newport, NH", FavoriteFood = "Poutine"},
             };
             public Student GetStudent(int id)
             {
-                return Directory.FirstOrDefault(e => e.Id == id);
+                return directory.FirstOrDefault(e => e.Id == id);
             }
         }
     }
